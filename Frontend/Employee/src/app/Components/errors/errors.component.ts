a55136eb4d23cd0de4ae3b94e5448bf1a5d2d649
MarwanMohamed-1 <marwanmuhammedd11@gmail.com>
2024-11-26 14:53:26 +0200

    Error Page

--- Frontend/Employee/src/app/Components/errors/errors.component.ts
+++ Frontend/Employee/src/app/Components/errors/errors.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-errors',
   standalone: true,
   imports: [],
   templateUrl: './errors.component.html',
-  styles: ``
+  styles: []
 })
 export class ErrorsComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back(); // Correct method to go back to the previous page
+  }
+
+  reloadPage(): void {
+    window.location.reload(); // Reloads the current page
+  }
 }
